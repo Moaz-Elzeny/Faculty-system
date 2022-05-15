93e156322976775c76f85c7aab1646a9a14c41f2
Moaz Elzeny <moazelzeny11@gmail.com>
2022-05-15 07:32:18 +0200

    initial commit

---
+++ ProjectMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using ProjectMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        FacultyEntities db = new FacultyEntities();
+        public ActionResult Index()
+        {
+            List<Department> list = db.Departments.ToList();
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Students(int id)
+        {
+            List<Student> studintList = db.Students.Where(x => x.Deptid == id).ToList();
+            return View(studintList);
+        }
+
+        public ActionResult Contact()
+        {
+            WebsiteDesc obj =  db.WebsiteDescs.FirstOrDefault();
+
+            return View(obj);
+        }
+    }
+}
